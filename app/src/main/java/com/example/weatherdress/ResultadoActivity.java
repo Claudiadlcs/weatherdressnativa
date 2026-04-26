0aee8890dfb3988c25e9fc06f7892908b2ebf8cb
Álvaro de la Cruz S <alvaro.dlcruzs@iesfm.onmicrosoft.com>
2026-04-26 16:10:55 +0200

    Parte 1 Claudia

--- app/src/main/java/com/example/weatherdress/ResultadoActivity.java
+++ app/src/main/java/com/example/weatherdress/ResultadoActivity.java
@@ -1,12 +1,12 @@
-package com.example.weatherdress; // Asegúrate de que este nombre coincida con el tuyo
+package com.example.weatherdress;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class ResultadoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_resultado);
     }
- }
+}
